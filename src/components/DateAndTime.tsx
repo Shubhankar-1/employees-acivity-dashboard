c656a287cdb2c7e50a9d2c22f4eb3fb198cd89d0
Shubhankar Nautiyal <shubhankar.nautiyal21@gmail.com>
2024-05-26 18:35:45 +0530

    Added DateAndTime component

---
+++ src/components/DateAndTime.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function DateAndTime() {
+    const currentDate = new Date().toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
+    const currentTime = new Date().toLocaleTimeString('en-US', { hour: '2-digit', minute: 'numeric' });
+
+    return (
+        <div className="flex flex-col w-max items-end font-semibold">
+            {/* Current Date */}
+            <div>{currentDate}</div>
+            
+            {/* Current Time */}
+            <div>{currentTime}</div>
+        </div>
+    );
+}
+    
